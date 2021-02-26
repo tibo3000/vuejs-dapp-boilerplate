9e0780d64630cc1bf9f411346f1ce041a8bbe544
Thibault Vincent-Ducimetiere <thibaultvd@voodoo.io>
2021-02-26 19:24:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vue.js dApp Boilerplate
+
+Based on https://github.com/dappuniversity/starter_kit
+
+## Usage
+```
+git clone https://github.com/tibo3000/vuejs-dapp-boilerplate.git
+npm i
+npm run dev
+```
+Enjoy :)
---
+++ src/pages/Hello.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg: 'Hello'
+        }
+    },
+    methods: {
+
+    },
+    mounted () {
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../mixins/style.less';
+
+h1 {
+    padding-top: 30vh;
+    font-size: 20vh;
+    text-align: center;
+}
+</style>
